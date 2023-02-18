e817f04aacafec2c5eef61fd3d5a87e094cafa86
mingyo0125 <wjdalsry125@gmail.com>
2023-02-18 15:46:08 +0900

    2023_02_18(Class_확장클래스)

---
+++ 2022~2023겨울방학(C#)/2023_02_18(Class4)/2023_02_18(Class4)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _2023_02_18_Class4_; //확장클래스
+
+namespace _2023_02_18_Class4_
+{
+    public static class Calculator
+    {
+        public static int Multifly(this int firstNum, int secondNum) //확장클래스
+        {
+            return firstNum * secondNum;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($" 3 * 4 = {3.Multifly(4)}"); //확장클래스
+        }
+    }
+}
