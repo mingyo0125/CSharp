c75bfe62eed7749cad903abbbfa97391224ce0ce
mingyo0125 <wjdalsry125@gmail.com>
2023-02-20 19:56:00 +0900

    2023_02_20(abstract)

---
+++ 2022~2023겨울방학(C#)/2023_02_20(abstract)/2023_02_20(abstract)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_02_20_abstract_
+{
+    internal class Program
+    {
+        abstract class AbstractBase
+        {
+            public abstract void AbstractMethod();
+        }
+
+        class Derived : AbstractBase
+        {
+            public override void AbstractMethod()
+            {
+                Console.WriteLine("Dervied.AbstractMethod");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            AbstractBase derived = new Derived();
+            derived.AbstractMethod();
+        }
+    }
+}
