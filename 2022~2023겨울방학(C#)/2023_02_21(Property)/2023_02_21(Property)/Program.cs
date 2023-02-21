e2a339e11e6fb6a05fdccc472e77721a30aaef92
mingyo0125 <wjdalsry125@gmail.com>
2023-02-21 15:29:19 +0900

    2023_02_21(Property)

---
+++ 2022~2023겨울방학(C#)/2023_02_21(Property)/2023_02_21(Property)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_02_21_Property_
+{
+    internal class Program
+    {
+        class Friends
+        {
+            private int age;
+            public int Age
+            {
+                get
+                {
+                    return age;
+                }
+                set
+                {
+                    age = value;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Friends friends = new Friends();
+            Console.WriteLine(friends.Age = 5);
+        }
+    }
+}
