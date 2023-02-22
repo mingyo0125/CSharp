de326b14a583631998e5f36586bfdf0b0f777ac9
mingyo0125 <wjdalsry125@gmail.com>
2023-02-22 22:24:08 +0900

    2023_02_22(Property_무명형식)

---
+++ 2022~2023겨울방학(C#)/2023_02_22(Property2)/2023_02_22(Property2)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_02_22_Property2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new { Name = "안형주", Age = 18 }; //무명형식
+            Console.WriteLine($"이름: {student.Name} \n나이: {student.Age}");
+        }
+    }
+}
