dae17ed5cc589e9833f2bbccbf6b3f86999e0c00
mingyo0125 <wjdalsry125@gmail.com>
2023-02-22 22:56:29 +0900

    2023_02_22(Property_추상클래스의 프로퍼티)

--- 2022~2023겨울방학(C#)/2023_02_22(Property2)/2023_02_22(Property2)/Program.cs
+++ 2022~2023겨울방학(C#)/2023_02_22(Property2)/2023_02_22(Property2)/Program.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace _2023_02_22_Property2_
 {
+    
+
+    abstract class Parents
+    {
+        public abstract int Age { get; set;}
+        public abstract string Name { get; set; }
+    }
+
+    #region 인터페이스의 프로퍼티
     interface ITeacher //인터페이스의 프로퍼티
     {
         public int Age { get; set; }
@@ -34,6 +43,13 @@ namespace _2023_02_22_Property2_
             }
         }
     }
+    #endregion
+
+    class _Person : Parents
+    {
+        public override int Age { get; set; }
+        public override string Name { get; set; }
+    }
 
     internal class Program
     {
@@ -41,6 +57,11 @@ namespace _2023_02_22_Property2_
         {
             var student = new { Name = "안형주", Age = 18 }; //무명형식
             Console.WriteLine($"이름: {student.Name} \n나이: {student.Age}");
+
+            Parents _Person = new _Person();
+            _Person.Name = "안형주";
+            _Person.Age = 18;
+            Console.WriteLine($"이름: {_Person.Name} \n나이: {_Person.Age}");
         }
     }
 }
