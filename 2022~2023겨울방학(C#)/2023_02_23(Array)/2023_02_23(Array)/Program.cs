a4c935783664a066121d3aad0c9158c8ae1f5e07
mingyo0125 <wjdalsry125@gmail.com>
2023-02-23 23:21:42 +0900

    2023_02_23(Array)

---
+++ 2022~2023겨울방학(C#)/2023_02_23(Array)/2023_02_23(Array)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_02_23_Array_
+{
+    struct Test
+    {
+        public int[] scores;
+
+        public Test(int index)
+        {
+            scores = new int[index];
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test(5);
+            foreach (var i in test.scores)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
