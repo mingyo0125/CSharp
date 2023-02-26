99a780d99cec200111c72c52e85b5f46e87ea7d9
mingyo0125 <wjdalsry125@gmail.com>
2023-02-26 23:43:44 +0900

    2023_02_26(Array_ArrayList)

---
+++ 2022~2023겨울방학(C#)/2023_02_26(Array)/2023_02_26(Array)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_02_26_Array_
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region Arraylist
+            ArrayList Arlist = new ArrayList();
+            Arlist.Add(1);
+            Arlist.Add(2);
+            Arlist.Add(3);
+            Arlist.Insert(3,2);
+            Arlist.RemoveAt(2);
+            #endregion
+        }
+    }
+}
