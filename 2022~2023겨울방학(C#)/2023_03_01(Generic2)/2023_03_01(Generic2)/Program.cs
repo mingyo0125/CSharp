bd480681b29208e01a07110d5c54f215593e2b0c
mingyo0125 <wjdalsry125@gmail.com>
2023-03-01 22:23:27 +0900

    2023_03_01(Generic_List)

---
+++ 2022~2023겨울방학(C#)/2023_03_01(Generic2)/2023_03_01(Generic2)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_03_01_Generic2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region List
+            List<string> list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            for(int i  = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+                if(list.Count-1 == i)
+                {
+                    Console.WriteLine("끝");
+                }
+            }
+            #endregion
+        }
+    }
+}
