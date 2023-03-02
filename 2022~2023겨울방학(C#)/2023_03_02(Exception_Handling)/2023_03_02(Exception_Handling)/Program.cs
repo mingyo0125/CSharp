507741f77b82e5171fd05f0c873f084afd36c2b9
mingyo0125 <wjdalsry125@gmail.com>
2023-03-02 22:01:22 +0900

    2023_03_02(Exception Handling_Try)

---
+++ 2022~2023겨울방학(C#)/2023_03_02(Exception_Handling)/2023_03_02(Exception_Handling)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_03_02_Exception_Handling_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("홀수를 입력하세요");
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+                if(input % 2 == 0)
+                {
+                    throw new Exception("짝수입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("어 잘했어");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
