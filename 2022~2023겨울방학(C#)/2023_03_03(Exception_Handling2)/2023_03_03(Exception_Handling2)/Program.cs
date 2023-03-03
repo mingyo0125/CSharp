8aa1f1252731cf427467e3f5f480a15839f7c135
mingyo0125 <wjdalsry125@gmail.com>
2023-03-03 22:05:13 +0900

    2023_03_03(ExceptionHandling_사용자 정의 예외 클래스)

---
+++ 2022~2023겨울방학(C#)/2023_03_03(Exception_Handling2)/2023_03_03(Exception_Handling2)/Program.cs
@@ -0,0 +1,39 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_03_03_Exception_Handling2_
+{
+
+    class FilterableException : Exception //사용자 정의 예외 클래스
+    {
+        public string Error { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("0 ~ 10사이의 숫자를 입력해주세요");
+            int input = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if( input > 10 || input < 0)
+                {
+                    throw new FilterableException() {Error = "0~10까지의 수가 아닙니다" };
+                }
+                else
+                {
+                    Console.WriteLine($"입력한 수 : {input}");
+                }
+            }
+            catch(FilterableException filter)
+            {
+                Console.WriteLine(filter.Error);
+            }
+
+        }
+    }
+}
