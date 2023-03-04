925841dcb10eb2c50a5b86fe834cb23be37f95d7
mingyo0125 <wjdalsry125@gmail.com>
2023-03-04 23:15:47 +0900

    2023_03_04(Delegate)

---
+++ 2022~2023겨울방학(C#)/2023_03_04(Delegate)/2023_03_04(Delegate)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_03_04_Delegate_
+{
+
+    internal class Program
+    {
+
+        delegate int Calculator(int a, int b);
+
+        static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        static void Main(string[] args)
+        {
+            Calculator calculator;
+            calculator= new Calculator(Plus);
+            Console.WriteLine(calculator(5,3)); //Plus
+
+            calculator = new Calculator(Minus);
+            Console.WriteLine(calculator(5, 3)); //Minus
+        }
+    }
+}
