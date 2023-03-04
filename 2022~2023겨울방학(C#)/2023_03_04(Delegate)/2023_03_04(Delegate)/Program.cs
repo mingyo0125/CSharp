6ce442c6f0538fdfc060fe86876d618749190bb5
mingyo0125 <wjdalsry125@gmail.com>
2023-03-04 23:51:40 +0900

    2023_03_04(Delegate_대리자체인)

--- 2022~2023겨울방학(C#)/2023_03_04(Delegate)/2023_03_04(Delegate)/Program.cs
+++ 2022~2023겨울방학(C#)/2023_03_04(Delegate)/2023_03_04(Delegate)/Program.cs
@@ -10,36 +10,43 @@ namespace _2023_03_04_Delegate_
     internal class Program
     {
 
-        delegate int Calculator(int a, int b); //delegate
-        delegate int Generic<T>(T a, T b); //일반화 대리자
+        delegate void Calculator(int a, int b); //delegate
+        delegate void Generic<T>(T a, T b); //일반화 대리자
 
-        static int Plus(int a, int b)
+        static void Plus(int a, int b)
         {
-            return a + b;
+            Console.WriteLine($"{a} + {b} = {a + b}");
         }
 
-        static int Minus(int a, int b)
+        static void Minus(int a, int b)
         {
-            return a - b;
+            Console.WriteLine($"{a} - {b} = {a - b}");
         }
 
-        static int Multyply<T>(int a, int b)
+        static void Multyply<T>(int a, int b)
         {
-            return a * b;
+            Console.WriteLine($"{a} * {b} = {a * b}");
         }
 
         
         static void Main(string[] args)
         {
             Calculator calculator;
-            calculator= new Calculator(Plus);
-            Console.WriteLine(calculator(5,3)); //Plus
+            /*
+            calculator = new Calculator(Plus);
+            calculator(5, 3); //Plus
 
             calculator = new Calculator(Minus);
-            Console.WriteLine(calculator(5, 3)); //Minus
+            calculator(5, 3);
+            */
+            calculator = new Calculator(Plus);
+            calculator += new Calculator(Minus);
+            calculator(5, 3); //대리자 체인
+            
 
             Generic<int> generic = new Generic<int>(Multyply<int>);
-            Console.WriteLine(generic(5,3)); //Multifly(Generic)
+            generic(5,3); //Multifly(Generic)
+
         }
     }
 }
