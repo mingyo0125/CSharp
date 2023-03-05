ec58d9e64590bc21ac9ea829efa46ae1cc417cb3
mingyo0125 <wjdalsry125@gmail.com>
2023-03-05 22:53:14 +0900

    2023_03_05(Delegate_Event)

---
+++ 2022~2023겨울방학(C#)/2023_03_05(Delegate2)/2023_03_05(Delegate2)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_03_05_Delegate2_
+{
+    internal class Program
+    {
+        delegate void EventHandler(string msg);
+
+        class MyNotifier
+        {
+            public event EventHandler SomethingHappened;
+            
+
+            public void Dosomething()
+            {
+                Console.WriteLine("Dosomething 실행됨");
+            }
+        }
+
+        static public void MyHandler(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void Main(string[] args)
+        {
+            MyNotifier notifier = new MyNotifier();
+            notifier.SomethingHappened += new EventHandler(MyHandler);
+            notifier.Dosomething();
+        }
+
+    }
+}
