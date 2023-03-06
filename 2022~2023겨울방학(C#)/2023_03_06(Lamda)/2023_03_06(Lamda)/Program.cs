9f6d71550eaba38f2b4fb9cbd315a8435a71f931
mingyo0125 <wjdalsry125@gmail.com>
2023-03-06 20:20:04 +0900

    2023_03_06(Lamda_문형식의 람다)

--- 2022~2023겨울방학(C#)/2023_03_06(Lamda)/2023_03_06(Lamda)/Program.cs
+++ 2022~2023겨울방학(C#)/2023_03_06(Lamda)/2023_03_06(Lamda)/Program.cs
@@ -9,11 +9,23 @@ namespace _2023_03_06_Lamda_
     internal class Program
     {
         delegate int Calculate(int a, int b);
+        delegate void DoSomething(string msg);
+
         static void Main(string[] args)
         {
             //Calculate calculate = (int a, int b) => a + b; //람다
 
             Calculate calculate = (a,b) => a + b; //형식유추람다
+            Console.WriteLine(calculate(3,3));
+
+
+
+            DoSomething doSomething = (string msg) => //문 형식의 람다식
+            {
+                Console.WriteLine(msg);
+            };
+
+            doSomething("문 형식의 람다식"); //문 형식의 람다식
         }
     }
 }
