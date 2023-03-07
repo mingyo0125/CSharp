d5fe330b99fae517ba840a980465812166e2c288
mingyo0125 <wjdalsry125@gmail.com>
2023-03-07 20:23:22 +0900

    2023_03_07(Lamda_Func)

---
+++ 2022~2023겨울방학(C#)/2023_03_07(Lamda2)/2023_03_07(Lamda2)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_03_07_Lamda2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int> fun = () => 10; //매개변수 없는 Fun
+            Func<int, int, int> Plus = (a, b) => a + b; //매개변수 있는 Fun
+
+            Console.WriteLine(fun());
+            Console.WriteLine(Plus(6,6));
+        }
+    }
+}
