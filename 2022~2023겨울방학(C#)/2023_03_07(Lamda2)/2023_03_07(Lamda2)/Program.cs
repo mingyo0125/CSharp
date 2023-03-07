26b7cd4e8acb4c0f820f7c18468caad170e7554f
mingyo0125 <wjdalsry125@gmail.com>
2023-03-07 20:26:37 +0900

    2023_03_07(Lamda_Action)

--- 2022~2023겨울방학(C#)/2023_03_07(Lamda2)/2023_03_07(Lamda2)/Program.cs
+++ 2022~2023겨울방학(C#)/2023_03_07(Lamda2)/2023_03_07(Lamda2)/Program.cs
@@ -11,10 +11,16 @@ namespace _2023_03_07_Lamda2_
         static void Main(string[] args)
         {
             Func<int> fun = () => 10; //매개변수 없는 Fun
-            Func<int, int, int> Plus = (a, b) => a + b; //매개변수 있는 Fun
+            Func<int, int, int> FPlus = (a, b) => a + b; //매개변수 있는 Fun
 
             Console.WriteLine(fun());
-            Console.WriteLine(Plus(6,6));
+            Console.WriteLine(FPlus(6,6));
+
+            Action action = () => Console.WriteLine(14); //매개변수 없는 Action
+            Action<int, int> APlus = (a,b) => Console.WriteLine(a + b); //매개변수 있는 Action
+
+            action();
+            APlus(8,8);
         }
     }
 }
